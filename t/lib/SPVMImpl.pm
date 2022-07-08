2e80b94ed2a926a67d54a687196af6d9876f6efd
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-07-08 13:46:40 +0900

    Add comment out oo interface

--- t/lib/SPVMImpl.pm
+++ t/lib/SPVMImpl.pm
@@ -69,4 +69,57 @@ use warnings::register;
   return $output->to_bin;
 };
 
+=pod
+*Digest::MD5::new = sub {
+  my $md5 = SPVM::Digest::MD5->new;
+  
+  return $md5;
+};
+
+*Digest::MD5::add = sub {
+  my ($self, @args) = @_;
+  
+  if ($^W || warnings::enabled()) {
+    if (defined $args[0] && $args[0] eq 'Digest::MD5') {
+      warn "Digest::MD5::md5 function probably called as class method";
+    }
+  }
+  
+  my $data = join('', @args);
+
+  if ($] >= 5.006) {
+    utf8::downgrade($data);
+  }
+  
+  my $md5 = SPVM::Digest::MD5->add($data);
+  
+  return $md5;
+};
+
+*Digest::MD5::digest = sub {
+  my ($self) = @_;
+  
+  my $output = $self->digest;
+  
+  return $output->to_bin;
+};
+
+*Digest::MD5::hexdigest = sub {
+  my ($self) = @_;
+  
+  my $output = $self->hexdigest;
+  
+  return $output->to_bin;
+};
+
+*Digest::MD5::b64digest = sub {
+  my ($self) = @_;
+  
+  my $output = $self->b64digest;
+  
+  return $output->to_bin;
+};
+
+=cut
+
 *Digest::MD5::is_spvm = sub { 1 };
