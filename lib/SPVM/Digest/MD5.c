d899ecdfa7bf2312797d51be13f73f413761c998
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-04-27 08:49:43 +0900

    Simplify comments

--- lib/SPVM/Digest/MD5.c
+++ lib/SPVM/Digest/MD5.c
@@ -1,6 +1,8 @@
 // Copyright (c) 2023 Yuki Kimoto
 // MIT License
 
+// The following codes was originally created by copying the MD5.xs in https://metacpan.org/pod/Digest::MD5
+
 #include "spvm_native.h"
 
 #include <string.h>
@@ -8,48 +10,6 @@
 
 const char* FILE_NAME = "SPVM/Digest/MD5.c";
 
-/* 
- * Originally
-
- * This library is free software; you can redistribute it and/or
- * modify it under the same terms as Perl itself.
- * 
- *  Copyright 1998-2000 Gisle Aas.
- *  Copyright 1995-1996 Neil Winton.
- *  Copyright 1991-1992 RSA Data Security, Inc.
- *
- * This code is derived from Neil Winton's MD5-1.7 Perl module, which in
- * turn is derived from the reference implementation in RFC 1321 which
- * comes with this message:
- *
- * Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
- * rights reserved.
- *
- * License to copy and use this software is granted provided that it
- * is identified as the "RSA Data Security, Inc. MD5 Message-Digest
- * Algorithm" in all material mentioning or referencing this software
- * or this function.
- *
- * License is also granted to make and use derivative works provided
- * that such works are identified as "derived from the RSA Data
- * Security, Inc. MD5 Message-Digest Algorithm" in all material
- * mentioning or referencing the derived work.
- *
- * RSA Data Security, Inc. makes no representations concerning either
- * the merchantability of this software or the suitability of this
- * software for any particular purpose. It is provided "as is"
- * without express or implied warranty of any kind.
- *
- * These notices must be retained in any copies of any part of this
- * documentation and/or software.
- */
-
-/* Perl does not guarantee that uint32_t is exactly 32 bits.  Some system
- * has no integral type with exactly 32 bits.  For instance, A Cray has
- * short, int and long all at 64 bits so we need to apply this macro
- * to reduce uint32_t values to 32 bits at appropriate places. If uint32_t
- * really does have 32 bits then this is a no-op.
- */
 #if BYTEORDER > 0x4321 || defined(TRUNCATE_uint32_t)
   #define TO32(x)    ((x) &  0xFFFFffff)
   #define TRUNC32(x) ((x) &= 0xFFFFffff)
