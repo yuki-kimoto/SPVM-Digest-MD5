c13266ccb50b37bcbdc1c81bc9d04615a6362643
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-04-27 09:20:52 +0900

    Improved the pod

--- lib/SPVM/Digest/MD5.pm
+++ lib/SPVM/Digest/MD5.pm
@@ -6,7 +6,11 @@ our $VERSION = "1.001";
 
 =head1 Name
 
-SPVM::Digest::MD5 - SPVM interface to the MD5 Algorithm
+SPVM::Digest::MD5 - MD5
+
+=head1 Description
+
+The Digest::MD5 class of L<SPVM> has methods for MD5.
 
 =head1 Usage
 
@@ -15,8 +19,9 @@ SPVM::Digest::MD5 - SPVM interface to the MD5 Algorithm
   my $digest = Digest::MD5->md5($data);
   my $digest = Digest::MD5->md5_hex($data);
   my $digest = Digest::MD5->md5_base64($data);
-  
-  # OO interface
+
+Object-Oriented Programming:
+
   my $md5 = Digest::MD5->new;
   
   $md5->add($data1);
@@ -26,44 +31,29 @@ SPVM::Digest::MD5 - SPVM interface to the MD5 Algorithm
   my $digest = $md5->hexdigest;
   my $digest = $md5->b64digest;
 
-=head1 Description
-
-The C<Digest::MD5> module allows you to use the RSA Data Security
-Inc. MD5 Message Digest algorithm from within Perl programs.  The
-algorithm takes as input a message of arbitrary length and produces as
-output a 128-bit "fingerprint" or "message digest" of the input.
-
-C<Digest::MD5> is Perl's L<Digest::MD5> porting to L<SPVM>.
-
-C<Digest::MD5> is a L<SPVM> module.
-
-=head1 Caution
-
-L<SPVM> is yet experimental status.
-
 =head1 Class Methods
 
 =head2 md5
 
-  static method md5 : string ($data : string)
+  static method md5 : string ($data : string);
 
-This function will concatenate all arguments, calculate the MD5 digest
-of this "message", and return it in binary form.  The returned string
+Calculates the MD5 digest
+of the $data, and returns it in binary form.  The returned string
 will be 16 bytes long.
 
 =head2 md5_hex
 
-  static method md5_hex : string ($data : string)
+  static method md5_hex : string ($data : string);
 
-Same as md5(), but will return the digest in hexadecimal form. The
+Same as L</"md5">, but will return the digest in hexadecimal form. The
 length of the returned string will be 32 and it will only contain
 characters from this set: '0'..'9' and 'a'..'f'.
 
 =head2 md5_base64
 
-  static method md5_base64 : string ($data : string)
+  static method md5_base64 : string ($data : string);
 
-Same as md5(), but will return the digest as a base64 encoded string.
+Same as L</"md5">, but will return the digest as a base64 encoded string.
 The length of the returned string will be 22 and it will only contain
 characters from this set: 'A'..'Z', 'a'..'z', '0'..'9', '+' and
 '/'.
@@ -77,7 +67,7 @@ string "==" to the result.
 
   static method new : Digest::MD5 ();
 
-The constructor returns a new C<Digest::MD5> object which encapsulate
+Returns a new L<Digest::MD5|SPVM::Digest::MD5> object which encapsulate
 the state of the MD5 message-digest algorithm.
 
 =head1 Instance Methods
@@ -87,27 +77,25 @@ the state of the MD5 message-digest algorithm.
   method add : Digest::MD5 ($data : string);
 
 The $data provided as argument are appended to the message we
-calculate the digest for.  The return value is the $md5 object itself.
+calculate the digest for. The return value is the L<Digest::MD5|SPVM::Digest::MD5> object itself.
 
-All these lines will have the same effect on the state of the $md5
-object:
+Examples:
 
-    $md5->add("a"); $md5->add("b"); $md5->add("c");
-    $md5->add("a")->add("b")->add("c");
-    $md5->add("abc");
+  $md5->add("abc");
+  $md5->add("efg");
 
 =head2 digest
 
   method digest : string ();
 
-Return the binary digest for the message.  The returned string will be
+Returns the binary digest for the message.  The returned string will be
 16 bytes long.
 
 =head2 hexdigest
 
   method hexdigest : string ();
 
-Same as $md5->digest, but will return the digest in hexadecimal
+Same as L</"digest">, but will return the digest in hexadecimal
 form. The length of the returned string will be 32 and it will only
 contain characters from this set: '0'..'9' and 'a'..'f'.
 
@@ -115,11 +103,10 @@ contain characters from this set: '0'..'9' and 'a'..'f'.
 
   method b64digest : string ();
 
-Same as $md5->digest, but will return the digest as a base64 encoded
+Same as L</"digest">, but will return the digest as a base64 encoded
 string.  The length of the returned string will be 22 and it will only
 contain characters from this set: 'A'..'Z', 'a'..'z', '0'..'9', '+'
 and '/'.
-
 
 The base64 encoded string returned is not padded to be a multiple of 4
 bytes long.  If you want interoperability with other base64 encoded
@@ -128,6 +115,14 @@ md5 digests you might want to append the string "==" to the result.
 =head1 Repository
 
 L<SPVM::Digest::MD5 - Github|https://github.com/yuki-kimoto/SPVM-Digest-MD5>
+
+=head1 See Also
+
+=over 2
+
+=item * L<Digest::MD5> - SPVM::Digest::MD5 is a Perl's Digest::MD5 porting to SPVM
+
+=back
 
 =head1 Author
 
@@ -142,4 +137,3 @@ L<Yoshiyuki Itoh|https://github.com/YoshiyukiItoh>
 Copyright (c) 2023 Yuki Kimoto
 
 MIT License
-
