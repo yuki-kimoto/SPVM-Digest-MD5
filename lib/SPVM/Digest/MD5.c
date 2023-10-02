70895fcca44488e4455d4143c9089c931a953191
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-10-02 09:32:03 +0900

    Use new native APIs.

--- lib/SPVM/Digest/MD5.c
+++ lib/SPVM/Digest/MD5.c
@@ -420,7 +420,7 @@ int32_t SPVM__Digest__MD5__new(SPVM_ENV* env, SPVM_VALUE* stack) {
   void* obj_self = env->new_object_by_name(env, stack, "Digest::MD5", &e, __func__, FILE_NAME, __LINE__);
   if (e) { return e; }
   
-  MD5_CTX* ctx = env->new_memory_stack(env, stack, sizeof(MD5_CTX));
+  MD5_CTX* ctx = env->new_memory_block(env, stack, sizeof(MD5_CTX));
   
   MD5Init(ctx);
   
@@ -446,7 +446,7 @@ int32_t SPVM__Digest__MD5__DESTROY(SPVM_ENV* env, SPVM_VALUE* stack) {
   MD5_CTX* ctx = env->get_pointer(env, stack, obj_ctx);
   assert(ctx);
   
-  env->free_memory_stack(env, stack, ctx);
+  env->free_memory_block(env, stack, ctx);
   
   return 0;
 }
